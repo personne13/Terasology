cfe820aba8f55c6b30480e2ab5aa7375ed7bf9d9
personne13 <thomasdamiani13@gmail.com>
2013-04-22 19:18:25 +0300

    Create MCLMDLFace

---
+++ src/main/java/org/terasology/rendering/assets/skeletalmesh/MCLMDLFace.java
@@ -0,0 +1,18 @@
+package org.terasology.rendering.assets.skeletalmesh;
+
+import com.bulletphysics.linearmath.QuaternionUtil;
+import com.google.common.collect.Lists;
+
+import javax.vecmath.Quat4f;
+import javax.vecmath.Vector3f;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * @author Thomas
+ */
+
+public class MCLMDLFace 
+{
+    private List<Vector3f> point = Lists.newArrayList();   
+}
